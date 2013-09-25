a424918ddeac58cad207438a5d717e3930b2f489
cc98 <goby@foxmail.com>
2013-09-25 10:42:59 +0800

    Create pat1011.cpp

---
+++ adv/pat1011.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<stdlib.h>
+#include<set>
+#include<bitset>
+#include<map>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<functional>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define RREP(I, N) for (int I = (N) - 1; I >= 0; --I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPC(I, C) for (int I = 0; !(C); ++I)
+#define REPCC(I,A, C) for (int I = (A); !(C); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+
+const char *dict="WTL";
+int main()
+{
+    float max[3] = {0}, tmp;
+    int index[3] = {0};
+    REP(i,3){
+        scanf("%f", &tmp);
+        if(max[0] < tmp){ max[0]=tmp;index[0]=i;}
+    }
+    REP(i,3){
+        scanf("%f", &tmp);
+        if(max[1] < tmp){ max[1]=tmp;index[1]=i;}
+    }
+    REP(i,3){
+        scanf("%f", &tmp);
+        if(max[2] < tmp){ max[2]=tmp;index[2]=i;}
+    }
+    REP(i,3){
+        printf("%c ",dict[index[i]]);
+    }
+    printf("%.2f", (max[0]*max[1]*max[2]*0.65-1)*2);
+    return 0;
+}

0ae878ab64153f50494eed2949e0652b0d0e64f7
cc98 <goby@foxmail.com>
2013-09-25 10:41:41 +0800

    Create pat1007.cpp

---
+++ adv/pat1007.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<stdlib.h>
+#include<set>
+#include<bitset>
+#include<map>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<functional>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPC(I, C) for (int I = 0; !(C); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+
+int main()
+{
+    int max[3],sum=0,tmp,start=-1,end;
+    bool first=true;
+    CASET{
+        DRI(num);
+        if(first) {max[0]=max[1]=max[2]=num;first=false;}
+        if(sum <= 0){sum=start=end=num;}
+        else{
+            tmp= sum+num ;
+            if(tmp<=0){sum=0;}
+            else {sum=tmp;end=num;}
+        }
+        if(sum > max[0]){max[0]=sum;max[1]=start;max[2]=end;}
+    }
+
+    printf("%d %d %d",max[0],max[1],max[2]);
+    return 0;
+}

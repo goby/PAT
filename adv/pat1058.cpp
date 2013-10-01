ac9349efffba62610030b3a8be909a51ad8c60d8
cc98 <goby@foxmail.com>
2013-10-01 22:32:08 +0800

    只敢做20分的题了

---
+++ adv/pat1058.cpp
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<stdlib.h>
+#include<set>
+#include<bitset>
+#include<map>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<functional>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define RREP(I, N) for (int I = (N) - 1; I >= 0; --I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPC(I, C) for (int I = 0; !(C); ++I)
+#define REPCC(I,A, C) for (int I = (A); !(C); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+
+
+int main()
+{
+    int G0,S0,K0,G1,S1,K1, flag=0;
+    scanf("%d.%d.%d %d.%d.%d",&G0,&S0,&K0, &G1,&S1,&K1);
+    
+    K0 += K1;
+    flag=K0/29;K0=K0%29;
+    S0 += S1+flag;
+    flag=S0/17;S0=S0%17;
+    G0 += G1+flag;
+    printf("%d.%d.%d", G0, S0, K0);
+    return 0;
+}

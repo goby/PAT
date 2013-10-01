a6f051e90a4da074c54ca4ffe4547c5afad12aa9
cc98 <goby@foxmail.com>
2013-10-02 01:58:08 +0800

    the last question with 20 cents...

---
+++ adv/pat1046.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<stdlib.h>
+#include<set>
+#include<bitset>
+#include<map>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<functional>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define RREP(I, N) for (int I = (N) - 1; I >= 0; --I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPC(I, C) for (int I = 0; !(C); ++I)
+#define REPCC(I,A, C) for (int I = (A); !(C); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first #define S second
+using namespace std;
+
+#define SIZE 100001
+int dist[SIZE]={0};
+
+int main()
+{
+    int total=0, tmp;
+    DRI(N);
+    REP(i, N) { RI(dist[i]);total+= dist[i]; dist[i]=total-dist[i];}
+    DRI(M);
+    REP(i, M){
+        DRII(x, y);
+        int dis = abs(dist[x-1]- dist[y-1]);
+        if(dis > total/2) printf("%d\n", total-dis);
+        else printf("%d\n", dis);
+    }
+    return 0;
+}

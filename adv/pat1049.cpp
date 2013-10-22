dffd1fd38fdb18a83aa013c74ac41785c6468180
cc98 <goby@foxmail.com>
2013-10-22 16:06:14 +0800

    Create pat1049.cpp

---
+++ adv/pat1049.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<stdlib.h>
+#include<set>
+#include<bitset>
+#include<map>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<limits.h>
+#include<functional>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define RREP(I, N) for (int I = (N) - 1; I >= 0; --I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPC(I, C) for (int I = 0; !(C); ++I)
+#define REPCC(I,A, C) for (int I = (A); !(C); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+
+#define SIZE 1000
+
+int coin[SIZE] = {0};
+
+int main()
+{
+    int tmp;
+    DRII(N,K);
+    REP(i, N){
+        RI(tmp);
+        coin[tmp]++;
+    }
+    int size = K / 2 + 1 ;
+    int i = 0;
+    for(;i < size; i++){
+        if(coin[i]-- && coin[K - i]){
+            printf("%d %d", i, K - i);
+            break;
+        }
+    }
+    if(i == size)
+        printf("No Solution");
+    return 0;
+}

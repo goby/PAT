2898bb10c68cb0cdf1fd3e2c3cddc6fc4c64c504
cc98 <goby@foxmail.com>
2013-10-23 00:48:06 +0800

    尽管状态不好，但是一击必杀的感觉还是超级好

---
+++ adv/pat1048.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string>
+#include<math.h>
+#include<stdlib.h>
+#include<set>
+#include<bitset>
+#include<map>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<limits.h>
+#include<functional>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define RREP(I, N) for (int I = (N) - 1; I >= 0; --I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define REPC(I, C) for (int I = 0; !(C); ++I)
+#define REPCC(I,A, C) for (int I = (A); !(C); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+
+char digit[20];
+
+int main()
+{
+    DRI(N);
+    
+    int result = 0;
+    int pre = 0;
+    int factor = 1;
+    while (N) {
+        int cur = N % 10;
+        if(cur == 1)
+            result += N / 10  * factor + pre + 1;
+        else if(cur == 0)
+            result += N / 10 * factor;
+        else
+            result += N / 10 * factor + factor;
+        pre += factor * cur;
+        factor *= 10;
+        N /= 10;
+    }
+    
+    printf("%d", result);
+    
+    return 0;
+}

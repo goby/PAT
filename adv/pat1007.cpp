ffb81c51eabf5c0d72b2ccdfb384a709f45c2cd4
cc98 <goby@foxmail.com>
2013-10-23 12:56:10 +0800

    Update pat1007.cpp

--- adv/pat1007.cpp
+++ adv/pat1007.cpp
@@ -10,12 +10,16 @@
 #include<algorithm>
 #include<iostream>
 #include<queue>
+#include<stack>
+#include<limits.h>
 #include<functional>
 #define SZ(X) ((int)(X).size())
 #define ALL(X) (X).begin(), (X).end()
 #define REP(I, N) for (int I = 0; I < (N); ++I)
+#define RREP(I, N) for (int I = (N) - 1; I >= 0; --I)
 #define REPP(I, A, B) for (int I = (A); I < (B); ++I)
 #define REPC(I, C) for (int I = 0; !(C); ++I)
+#define REPCC(I,A, C) for (int I = (A); !(C); ++I)
 #define RI(X) scanf("%d", &(X))
 #define RII(X, Y) scanf("%d%d", &(X), &(Y))
 #define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
@@ -35,20 +39,20 @@ using namespace std;
 
 int main()
 {
-    int max[3],sum=0,tmp,start=-1,end;
+    int max[3] = {0},sum=0,start=-1,end;
+    int n0;
     bool first=true;
+    max[0] = -1;
     CASET{
         DRI(num);
-        if(first) {max[0]=max[1]=max[2]=num;first=false;}
-        if(sum <= 0){sum=start=end=num;}
-        else{
-            tmp= sum+num ;
-            if(tmp<=0){sum=0;}
-            else {sum=tmp;end=num;}
-        }
+        end=num;
+        sum += num;
+        if(first) {n0=start=num;first=false;}
+        if(!start)start=num;
         if(sum > max[0]){max[0]=sum;max[1]=start;max[2]=end;}
+        if(sum <= 0){sum=0;start=0;}
     }
-
+    if(max[0]<0){max[0]=0;max[1] = n0;max[2]=end;}
     printf("%d %d %d",max[0],max[1],max[2]);
     return 0;
 }
